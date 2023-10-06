bd09eaa6959dfc18b9ce3e0123044fa5ac015ff2
HoldenFolk <holdenfolk1@gmail.com>
2023-10-06 15:23:35 -0400

    Add facial tracking and image resizing in the webcam viewer

--- src/webcamPredict.py
+++ src/webcamPredict.py
@@ -3,36 +3,50 @@ import numpy as np
 import tensorflow as tf
 
 # Load the model
-model = tf.keras.models.load_model("./assets/emotion_model.h5")
+model = tf.keras.models.load_model("./assets/emotion_model2.h5")
 emotions = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
 
 cap = cv2.VideoCapture(0)  # 0 for default camera
+
+# Load pre-trainned face detection model from open cv
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    # Convert to grayscale and resize to match the model's expected input shape
+    # Convert the frame to grayscale for face detection
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    resized_gray = cv2.resize(gray, (48, 48))
-    input_data = (resized_gray / 255.0).reshape(-1, 48, 48, 1)
 
-    # Predict emotion
-    predictions = model.predict(input_data)
-    emotion = emotions[np.argmax(predictions)]
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-    # Display the result
-    cv2.putText(
-        frame,
-        emotion,
-        (10, 50),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        2,
-        (0, 255, 0),
-        2,
-        cv2.LINE_AA,
-    )
+    for x, y, w, h in faces:
+        roi_gray = gray[y : y + h, x : x + w]  # Region of interest (the face)
+        resized_gray = cv2.resize(roi_gray, (48, 48))
+        input_data = (resized_gray / 255.0).reshape(-1, 48, 48, 1)
+
+        # Predict emotion
+        predictions = model.predict(input_data)
+        emotion = emotions[np.argmax(predictions)]
+
+        # Display the rectangle and predicted emotion
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(
+            frame,
+            emotion,
+            (x, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.9,
+            (0, 255, 0),
+            2,
+            cv2.LINE_AA,
+        )
+
     cv2.imshow("Emotion Detection", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
